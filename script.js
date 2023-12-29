3d631fcf96113b83b0dd701b0d82f37ae6ab1744
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-12-29 11:43:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 // complete the JS code
+
 const nameInput = document.getElementById("name");
 const scoreInput = document.getElementById("score");
 const scores = document.getElementById("scores");
+
+
+
+const tableData = []
 
 // Save score to Local Storage
 function saveScore() {
   // complete the code here
   showScores();
-}
+} 
 
 // Show scores in div
 function showScores() {
   // complete the code
+	console.log("scores")
+	scores.innerHTML = ''
+	const table = document.createElement('table')
+	table.innerHTML = "
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>Score</th>
+				</tr>
+			</thead>
+	"
+	scores.appendChild(table)
 }
